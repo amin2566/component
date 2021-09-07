3b521b87b39c27deb656651e6d36dcce89066c5f
amin2566 <aminalimi45@gmail.com>
2021-09-07 23:29:10 +0200

    added readme

---
+++ comp/src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+           <h1>Gafsa,Tunisia,Africa</h1> 
+        </div>
+    )
+}
+
+export default Address
---
+++ comp/src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import wejhi from './Image.jpg'
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={amin} />
+        </div>
+    )
+}
+
+export default ProfilePhoto
